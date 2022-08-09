8f2548accfe87db36a497976c0be7431f628a2dc
Denis <molatok@ya.ru>
2022-08-09 23:27:39 +0300

    Работает

--- service/movie.py
+++ service/movie.py
@@ -4,14 +4,13 @@ from dao.movie import MovieDAO
 class MovieService:
     def __init__(self, dao: MovieDAO):
         self.dao = dao
-        
-        
+    
     def get_movies(self, mid=None, **kwargs):
         return self.dao.get(mid, **kwargs)
-
+    
     def create_movie(self, data):
         return self.dao.create(data)
-
+    
     def update(self, movie_id, data):
         movie = self.get_movies(movie_id)
         movie.title = data['title']
@@ -23,17 +22,15 @@ class MovieService:
         self.dao.update(movie)
         return movie
         
-        
         self.session.add(movie)
         self.session.commit()
-
-
+    
     def delete(self, mid):
         movie = self.get(mid)
         if not movie:
             return
         self.session.delete(movie)
-
+    
     def filter_by_genre(self, genre_id):
         movies = self.get_movies()
         result = []
